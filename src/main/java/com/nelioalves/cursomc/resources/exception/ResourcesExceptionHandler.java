f645478cd6cd104d68cdca78e624a57171bdc082
Israel Pontes <israel.pontes.1920@gmail.com>
2021-08-14 22:08:04 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourcesExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourcesExceptionHandler.java
@@ -2,6 +2,7 @@ package com.nelioalves.cursomc.resources.exception;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.http.HttpStatus;
@@ -18,4 +19,11 @@ public class ResourcesExceptionHandler {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
+    
+    @ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -2,12 +2,14 @@ package com.nelioalves.cursomc.services;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class CategoriaService {
@@ -30,4 +32,14 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+    
+    public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+    }
 }
