fda33484f4830f9a6281e56df4ae81d896695fea
Israel Pontes <israel.pontes.1920@gmail.com>
2021-08-14 21:47:23 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/PedidoResource.java
@@ -1,17 +1,14 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.nelioalves.cursomc.domain.Pedido;
-import com.nelioalves.cursomc.services.PedidoService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.services.PedidoService;
 
 @RestController
 @RequestMapping(value = "/pedidos")
@@ -21,9 +18,8 @@ public class PedidoResource {
     private PedidoService service;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> find(@PathVariable Integer id) {
-        Pedido obj = service.find(id);
-        return ResponseEntity.ok().body(obj);
-
-    }
+    public ResponseEntity<Pedido> find(@PathVariable Integer id) {
+		Pedido obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
