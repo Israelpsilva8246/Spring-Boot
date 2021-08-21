0379454d8bacdcad5bcd40347dd117a4c31e6bde
Israel Pontes <israel.pontes.1920@gmail.com>
2021-08-21 10:13:49 -0300

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -40,35 +40,51 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 
 	public Produto getProduto() {
 		return id.getProduto();
 	}
 	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
 	public ItemPedidoPK getId() {
 		return id;
 	}
+	
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
+	
 	public double getDesconto() {
 		return desconto;
 	}
+	
 	public void setDesconto(double desconto) {
 		this.desconto = desconto;
 	}
+	
 	public Integer getQuantidade() {
 		return quantidade;
 	}
+	
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
+	
 	public double getPreco() {
 		return preco;
 	}
+	
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -76,6 +92,7 @@ public class ItemPedido implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
